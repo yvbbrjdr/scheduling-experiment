01ca5960a6433bc78a8d1d8dc2a2916a31fb4a86
yvbbrjdr <xyyqzy@hotmail.com>
2019-03-14 14:33:33 -0700

    stats script

---
+++ tools/stats.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import queue
+import re
+
+def main():
+    que = queue.Queue()
+    start_pattern = re.compile(r'^Starting time: (.*)$')
+    end_pattern = re.compile(r'^Ending time: (.*)$')
+    while True:
+        try:
+            line = input()
+        except:
+            break
+        start_match = start_pattern.match(line)
+        end_match = end_pattern.match(line)
+        if start_match:
+            que.put(float(start_match.group(1)))
+        elif end_match:
+            start = que.get()
+            end = float(end_match.group(1))
+            print('{}: {}'.format(start, end - start))
+
+if __name__ == '__main__':
+    main()
