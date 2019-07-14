5e7466835b332d0533fea24ae73de2e20f3cf2a3
yvbbrjdr <xyyqzy@hotmail.com>
2019-07-14 15:51:27 -0700

    fixes

--- tools/test.sh
+++ tools/test.sh
@@ -1,41 +1,25 @@
 #!/bin/bash
+set -euo pipefail
 
-if [ $# -eq 0 ]
-  then
+if [[ $# -eq 0 ]]; then
     echo "Need path to se"
-    exit
+    exit 1
 fi
 
-#variables
-END_THREAD_NUM=50
-THREAD_INCREMENTS=10
-END_RATE=100000000
-RATE_INCREMENTS=10000
-TEST_TIME=10
-
-#plan is to iterate through each of the modes and vary each quantity
-#execute for 10 seconds each
 PIPE_TYPES=(b s e u)
 PIN_MODES=(s r n)
-THREAD_NUM=0
-RATE=0
+PIPE_LEN=(2 4 8 16 100)
+RATES=(2000000 20000000)
 
 mkdir -p results
 
-for type in "${PIPE_TYPES[@]}"
-do
-    for mode in "${PIN_MODES[@]}"
-    do
-        for ((n=1;n<=END_THREAD_NUM;n+=THREAD_INCREMENTS))
-        do
-            for ((r=1;r<=END_RATE;r+=RATE_INCREMENTS))
-            do
-                echo "Executing $1 $type $mode $n $r"
+for type in "${PIPE_TYPES[@]}"; do
+    for mode in "${PIN_MODES[@]}"; do
+        for n in "${PIPE_LEN[@]}"; do
+            for r in "${RATES[@]}"; do
                 cmd="$1 $type $mode $n $r"
-                $cmd > "results/$type_$mode_$n_$r" &
-                TASK_PID=$!
-                sleep $TEST_TIME
-                kill $TASK_PID
+                echo "Executing $cmd"
+                timeout --preserve-status -s INT 2 $cmd > "results/${type}_${mode}_${n}_${r}"
             done
         done
     done
